5fc3bd3c6f48c5a00285cf5bc4f2dd8dbe01ebc4
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-14 03:14:15 +0000

    Dinosaur should have number of offspring per year test passed

--- dinosaur.js
+++ dinosaur.js
@@ -1,5 +1,6 @@
-const Dinosaur = function(type) {
+const Dinosaur = function(type, offspring_py) {
   this.type = type;
+  this.offspring_py = offspring_py;
 }
 
 module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -8,4 +8,10 @@ it('should have a type', function() {
   const actual = dinosaur.type;
   assert.strictEqual(actual, 'Velociraptor');
 })
+
+it('should have number of offspring per year', function () {
+  dinosaur = new Dinosaur('Velociraptor', 4);
+  const actual = dinosaur.offspring_py;
+  assert.strictEqual(actual,4);
+})
 })
