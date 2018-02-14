2c3f992bd8bc5d97bbc82c04a572c64d16cefec8
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-14 04:01:17 +0000

    Park should be able to add dinosaurs to it test passed

--- specs/park_test.js
+++ specs/park_test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const Park = require('../park');
+const Dinosaur = require('../dinosaur');
 
 describe('Park', function() {
 
@@ -8,5 +9,14 @@ describe('Park', function() {
     const actual = park.enclosure;
     assert.deepStrictEqual(actual, []);
   })
+
+  it('should be able to add dinosaurs to it', function() {
+    park = new Park();
+    dinosaur = new Dinosaur('T-Rex',1);
+    park.addToEnclosure(dinosaur);
+    const actual = park.enclosure.length;
+    assert.deepStrictEqual(actual, 1);
+  })
+
 
 })
