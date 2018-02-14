b5f818837f426848036716b2815431c70e4f610e
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-14 04:13:00 +0000

    created beforeEach in park_test.js

--- specs/park_test.js
+++ specs/park_test.js
@@ -4,18 +4,27 @@ const Dinosaur = require('../dinosaur');
 
 describe('Park', function() {
 
-  it('should start with an empty enclosure', function() {
+  let park;
+  let dinosaur;
+
+  beforeEach(function() {
     park = new Park();
+    trex = new Dinosaur('T-Rex',1);
+  })
+
+  it('should start with an empty enclosure', function() {
     const actual = park.enclosure;
     assert.deepStrictEqual(actual, []);
   })
 
   it('should be able to add dinosaurs to it', function() {
-    park = new Park();
-    dinosaur = new Dinosaur('T-Rex',1);
     park.addToEnclosure(dinosaur);
     const actual = park.enclosure.length;
     assert.deepStrictEqual(actual, 1);
+  })
+
+  it('should be able to remove all dinosaurs of a particular type', function() {
+
   })
 
 
